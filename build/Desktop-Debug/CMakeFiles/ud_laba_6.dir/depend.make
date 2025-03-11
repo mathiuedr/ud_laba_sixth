e45809d277603e4108203dc2db38c186c85180ba
mathiuedr <fuulpol@gmail.com>
2025-03-11 18:08:27 +0300

    minor cleaning

--- build/Desktop-Debug/CMakeFiles/ud_laba_6.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ud_laba_6.
-# This may be replaced when dependencies are built.
--- build/Desktop-Debug/CMakeFiles/ud_laba_6_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ud_laba_6_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ud_laba_6_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
